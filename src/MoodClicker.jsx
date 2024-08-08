5d39504b1d3c46da9de285f7f3e7558d5b34be51
danx <rushn24@gmail.com>
2024-08-07 21:21:44 -0400

    added light/dark mode feature

--- src/MoodClicker.jsx
+++ src/MoodClicker.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
+import './MoodClicker.css';
 
 const MoodClicker = () => {
-    const [isHappy, setIsHappy] = useState(true)
+    const [isHappy, setIsHappy] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useState(false);
     const toggleMood = () => {
-        setIsHappy(!isHappy)
-        // setIsHappy(mood => !mood) is another way using callback form
-    }
+        setIsHappy(!isHappy);
+        // setIsHappy(mood => !mood); is another way using callback form
+    };
+    const toggleDarkMode = () => {
+        setIsDarkMode(mode => !mode);
+    };
     return (
-        <div>
+        <div className={isDarkMode ? 'MoodClicker-dark' : 'MoodClicker-light'}>
         <h1>{isHappy ? '😊' : '😒'}</h1>
         <button onClick={toggleMood}>Change Mood</button>
+        <button onClick={toggleDarkMode}>{isDarkMode ? 'Light Mode' : 'Dark Mode'}</button>
         </div>
     );
 };
