55f5432faed9ace1d00228fdf9bddb93802a2c70
danx <rushn24@gmail.com>
2024-08-07 21:06:48 -0400

    added MoodClicker component

--- src/MoodClicker.jsx
+++ src/MoodClicker.jsx
@@ -2,11 +2,16 @@ import React, { useState } from 'react';
 
 const MoodClicker = () => {
     const [isHappy, setIsHappy] = useState(true)
-
+    const toggleMood = () => {
+        setIsHappy(!isHappy)
+        // setIsHappy(mood => !mood) is another way using callback form
+    }
     return (
         <div>
-        <h1>{isHappy ? '=)' : '=('}</h1>
-        <button onClick={changeMood}>Change Mood</button>
+        <h1>{isHappy ? '😊' : '😒'}</h1>
+        <button onClick={toggleMood}>Change Mood</button>
         </div>
-    )
-}
+    );
+};
+
+export default MoodClicker;
